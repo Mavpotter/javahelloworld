2fdf6f619521c01328fe45b01c513ab2de5db1f9
niclas <niclas@molgen.mpg.de>
2017-07-09 04:14:14 +0200

    variable size of boxes

--- Create3dSudokuSimpleBacktracking.java
+++ Create3dSudokuSimpleBacktracking.java
@@ -3,14 +3,24 @@
  */
 public class Create3dSudokuSimpleBacktracking
 {
+	private int n;
+	private int n2;
 	private int[][][] _3dSudoku; // x, y, z
 
 	/**
-	 * Creates an instance of an empty 3d-Sudoku.
+	 * Creates an instance of an empty 3d-Sudoku with 3x3 boxes.
 	 */
 	public Create3dSudokuSimpleBacktracking ()
 	{
-		init();
+		init(3);
+	}
+
+	/**
+	 * Creates an instance of an empty 3d-Sudoku with nxn boxes.
+	 */
+	public Create3dSudokuSimpleBacktracking (int n)
+	{
+		init(n);
 	}
 
 	/**
@@ -18,10 +28,10 @@ public class Create3dSudokuSimpleBacktracking
 	 */
 	public void print3dSudoku ()
 	{
-		for (int z = 0; z < 9; ++z){
+		for (int z = 0; z < n2; ++z){
 			System.out.println ("Ebene " + (z+1));
-			for (int y = 0; y < 9; ++y){
-				for (int x = 0; x < 9; ++x){
+			for (int y = 0; y < n2; ++y){
+				for (int x = 0; x < n2; ++x){
 					System.out.print (_3dSudoku[x][y][z] + " ");
 				} // for x
 				System.out.println ();
@@ -47,13 +57,15 @@ public class Create3dSudokuSimpleBacktracking
 		return _3dSudoku;
 	}
 
-	private void init ()
+	private void init (int n)
 	{
-		_3dSudoku = new int[9][9][9];
+		this.n = n;
+		n2 = n * n;
+		_3dSudoku = new int[n2][n2][n2];
 
-		for (int z = 0; z < 9; ++z)
-			for (int y = 0; y < 9; ++y)
-				for (int x = 0; x < 9; ++x)
+		for (int z = 0; z < n2; ++z)
+			for (int y = 0; y < n2; ++y)
+				for (int x = 0; x < n2; ++x)
 					_3dSudoku[x][y][z] = 0;
 	}
 
@@ -66,7 +78,7 @@ public class Create3dSudokuSimpleBacktracking
 
 	private boolean areRowsAndColumnsOk (int x, int y, int z)
 	{
-		for (int i = 0; i < 9; ++i)
+		for (int i = 0; i < n2; ++i)
 			if ((i != x && _3dSudoku[i][y][z] == _3dSudoku[x][y][z])
 				|| (i != y && _3dSudoku[x][i][z] == _3dSudoku[x][y][z])
 				|| (i != z && _3dSudoku[x][y][i] == _3dSudoku[x][y][z]))
@@ -76,12 +88,12 @@ public class Create3dSudokuSimpleBacktracking
 
 	private boolean areBoxesOk (int x, int y, int z)
 	{
-		int tx = x % 3;
-		int ty = y % 3;
-		int tz = z % 3;
+		int tx = x % n;
+		int ty = y % n;
+		int tz = z % n;
 
-		for (int j = 0; j < 3; ++j)
-			for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < n; ++j)
+			for (int i = 0; i < n; ++i)
 				if ((tx != i && ty != j && _3dSudoku[x-tx+i][y-ty+j][z] == _3dSudoku[x][y][z])
 					|| (tz != i && ty != j && _3dSudoku[x][y-ty+j][z-tz+i] == _3dSudoku[x][y][z])
 					|| (tx != i && tz != j && _3dSudoku[x-tx+i][y][z-tz+j] == _3dSudoku[x][y][z]))
@@ -91,20 +103,20 @@ public class Create3dSudokuSimpleBacktracking
 
 	private boolean backtrack (int x, int y, int z)
 	{
-		if (x == 9){ // check if line finished
+		if (x == n2){ // check if line finished
 			x = 0;
 			++y;
 		}
 
-		if (y == 9){ // check if plag finished
+		if (y == n2){ // check if plag finished
 			y = 0;
 			++z;
 		}
 
-		if (z == 9)
+		if (z == n2)
 			return true; // 3d-Sudoku created / solved
 
-		for (int i = 0; i < 9; ++i){
+		for (int i = 0; i < n2; ++i){
 			_3dSudoku[x][y][z] = i+1;
 			if (isLegal (x, y, z))
 				if (backtrack (x+1, y, z))
